c260ff072b87cf2556c852731627399c969697db
Jodie Albert <jodie.albert@mail.concordia.ca>
2022-02-07 21:43:24 -0500

    Update Data Cleaning.R

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -6,3 +6,10 @@ library(ggplot2)
 
 database1 = read.csv("~/Documents/grad school/Credit Rating Project/database1.csv")
 database2 = read.csv("~/Documents/grad school/Credit Rating Project/database2.csv")
+
+sum1 =summary(database1)
+sum2 =summary(database2)
+
+write.csv(sum1,'sum1.csv')
+write.csv(sum2,'sum2.csv')
+
