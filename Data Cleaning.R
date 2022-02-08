7ead9a0647a4851ff71dadd4d1c41f8b72455e59
Jodie Albert <jodie.albert@mail.concordia.ca>
2022-02-07 21:27:23 -0500

    Create Data Cleaning.R

---
+++ Data Cleaning.R
@@ -0,0 +1,8 @@
+rm(list = ls())
+
+library(data.table)
+library(reshape2)
+library(ggplot2)
+
+database1 = read.csv("~/Documents/grad school/Credit Rating Project/database1.csv")
+database2 = read.csv("~/Documents/grad school/Credit Rating Project/database2.csv")
